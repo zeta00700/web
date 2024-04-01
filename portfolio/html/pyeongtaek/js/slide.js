252b826b358581b24614cfff3bbd28729b0f9da9
zeta00700 <156274029+zeta00700@users.noreply.github.com>
2024-04-01 21:17:21 +0900

    Add files via upload

--- portfolio/html/pyeongtaek/js/slide.js
+++ portfolio/html/pyeongtaek/js/slide.js
@@ -1,10 +1,17 @@
 $(function(){
     
-
     let b = 0
     function slide(){
         if(b<4){b++;}else{b=0;};
         $('.slide ul').animate({left: -100 * b + "%"},1200);
     }
     setInterval (slide,7500);
+
+    let c = 0
+    function slide(){
+        if(c<2){c++;}else{c=0;};
+        $('.t_con_4 div ul').animate({left: -100 * c + "%"},1000);
+    }
+    setInterval (slide,3000);
+
 });
