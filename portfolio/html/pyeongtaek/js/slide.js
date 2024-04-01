c36dd002d46c9b61765c3fa84febf7232283bd6d
zeta00700 <156274029+zeta00700@users.noreply.github.com>
2024-04-01 22:30:09 +0900

    Add files via upload

--- portfolio/html/pyeongtaek/js/slide.js
+++ portfolio/html/pyeongtaek/js/slide.js
@@ -14,4 +14,18 @@ $(function(){
     }
     setInterval (slide,3000);
 
+    $('.num').each(function(){ //각각 .num클래스 기능 정의
+        $(this).prop('Counter',0).animate({ //카운터속성정의
+            Counter:$(this).text()}, //보고있는 숫자를 
+            {duration:2000, //2000ms만큼 
+            easing:'swing', //숫자를 돌린다
+            step:function(now){
+                $(this).text(Math.ceil(now));
+            } //html 입력된 최종 숫자까지,math.cell은 소수값이 존재할 때
+            //값을 올리는 역할을 하는 함수이며 math.floor는 소수값이 존재할 때
+            //소수값을 버리는 역할을 하는 함수이며 math.round는 소수값에 따라
+            //올리거나 버리는 역할을 하는 반올림 함수이빈다
+        });
+    });
+
 });
